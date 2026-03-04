3d69abdb67a8d6e6442345dadedb83484d7b1f81
Thắng Đồng <thangdong@gmail.com>
2026-03-04 09:08:24 +0700

    fix: localStorage not updating after deleting task

--- src/schedule.js
+++ src/schedule.js
@@ -12,10 +12,13 @@ const backupDB = require("@/schedules/backupDB");
 const cleanupExpiredTokens = require("@/schedules/cleanupExpiredTokens");
 
 // Daily report: 2h sáng mỗi ngày
+
 new CronJob("0 0 2 * * *", dailyReport, null, true);
 
 // Backup DB: 3h sáng mỗi ngày
+
 new CronJob("0 0 3 * * *", backupDB, null, true);
 
 // Cleanup expired tokens: 1h sáng mỗi ngày
+
 new CronJob("0 0 1 * * *", cleanupExpiredTokens, null, true);
